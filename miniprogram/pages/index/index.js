7316725eeae0cdd1d217c2400737e53a05c73f22
zhanghaichuan <924320752@qq.com>
2020-06-08 21:44:07 +0800

    稍微改了下UI

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,23 +8,23 @@ Page({
         url: 'bill/cud/cud?itemType=0'
       },
       {
-        text: '查询修理单',
-        url: 'bill/search/search?itemType=0'
-      },
-      {
         text: '新建采购单',
         url: 'bill/cud/cud?itemType=1'
+      },
+      {
+        text: '查询修理单',
+        url: 'bill/search/search?itemType=0'
       },
       {
         text: '查询采购单',
         url: 'bill/search/search?itemType=1'
       },
       {
-        text: '修理项目管理',
+        text: '管理修理项目',
         url: 'item/menu/menu?itemType=0'
       },
       {
-        text: '采购项目管理',
+        text: '管理采购项目',
         url: 'item/menu/menu?itemType=1'
       },
       {
