bafa7b1845121c77795825718dba2c5dd2e69623
zhanghaichuan <924320752@qq.com>
2020-06-15 22:49:25 +0800

    收支统计ing

--- cloudfunctions/period-amount/index.js
+++ cloudfunctions/period-amount/index.js
@@ -3,24 +3,21 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 const db = cloud.database()
-const _ = db.command
-const $ = _.aggregate
+const $ = db.command.aggregate
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  let date1 = new Date('2020-06-09 10:00:00 GMT +8')
-  let date2 = new Date('2020-06-09 18:00:00 GMT +8')
-  let what = db
-    .collection('repair-bill')
+  return db
+    .collection(event.collectionName)
     .aggregate()
     .project({
       // and 操作好像有问题，不论是比较date还是int，都不能得到正确的结果
       //（测试的结果是and返回值跟第一个表达式结果相同，而不是文档上写的“and 仅在所有表达式都返回 true 时返回 true，否则返回 false”）
       // 只好注释掉下面这行，把两个条件拆开，再到 match 里筛选
       // matched: $.and($.gte(['$date', date1]), $.lt(['$date', date2])),
-      
-      gte: $.gte(['$date', date1]),
-      lt: $.lt(['$date', date2]),
+
+      gte: $.gte(['$date', event.fromDate]),
+      lt: $.lt(['$date', event.toDate]),
       amount: '$amount',
     })
     .match({
@@ -32,6 +29,4 @@ exports.main = async (event, context) => {
       totalAmount: $.sum('$amount')
     })
     .end()
-
-  return what
 }
--- miniprogram/pages/statistics/statistics.js
+++ miniprogram/pages/statistics/statistics.js
@@ -12,16 +12,45 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.cloud.callFunction({
-      name: 'add_array',
-      success: res => {
-        console.log('[云函数] [add_array] 调用成功：', res.result)
-        console.log(res.result.list[0])
-      },
-      fail: err => {
-        console.error('[云函数] [add_array] 调用失败', err)
-      }
-    })
+    // let dateString = '2020-06-11'
+    // let fromDateString = dateString + ' 00:00:00 GMT +8'
+    // let toDateString = dateString + ' 23:59:59 GMT +8'
+    // wx.cloud.callFunction({
+    //   name: 'profit-in-month',
+    //   data: {
+    //     fromDate: new Date(fromDateString),
+    //     toDate: new Date(toDateString),
+    //   },
+    //   success: res => {
+    //     console.log('[云函数] [add_array] 调用成功：', res.result)
+    //   },
+    //   fail: err => {
+    //     console.error('[云函数] [add_array] 调用失败', err)
+    //   }
+    // })
+
+    // 承载所有操作的 promise 的数组
+    let tasks = []
+    let year = '2020', month = '6'
+    for (let i = 1; i < 30; i++) {
+      let dateString = year + '-' + month + '-' + i;
+      let fromDateString = dateString + ' 00:00:00 GMT +8'
+      let toDateString = dateString + ' 23:59:59 GMT +8'
+      const promise = wx.cloud.callFunction({
+        name: 'period-amount',
+        data: {
+          fromDate: new Date(fromDateString),
+          toDate: new Date(toDateString)
+        }
+      })
+      tasks.push(promise)
+    }
+    // 等待执行所有
+    Promise.all(tasks).then((values) => {
+      console.log(values[10].result.list[0].totalAmount);
+    });
+
+    // console.log(results)
   },
 
   /**
