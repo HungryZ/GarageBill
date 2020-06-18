c2fcddd93e0036f9f67cf37f5c441eb25327a712
zhanghaichuan <924320752@qq.com>
2020-06-18 23:05:08 +0800

    按月份统计完成

--- miniprogram/pages/statistics/statistics.js
+++ miniprogram/pages/statistics/statistics.js
@@ -5,100 +5,108 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    statisticList: [],
+    dateString: '未选择',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // let dateString = '2020-06-11'
-    // let fromDateString = dateString + ' 00:00:00 GMT +8'
-    // let toDateString = dateString + ' 23:59:59 GMT +8'
-    // wx.cloud.callFunction({
-    //   name: 'profit-in-month',
-    //   data: {
-    //     fromDate: new Date(fromDateString),
-    //     toDate: new Date(toDateString),
-    //   },
-    //   success: res => {
-    //     console.log('[云函数] [add_array] 调用成功：', res.result)
-    //   },
-    //   fail: err => {
-    //     console.error('[云函数] [add_array] 调用失败', err)
-    //   }
-    // })
 
+  },
+
+  bindDateChange(e) {
+    this.setData({
+      dateString: e.detail.value
+    })
+    let array = e.detail.value.split('-')
+    this.requestTotalForYearAndMonth(Number(array[0]), Number(array[1]))
+  },
+
+  requestTotalForYearAndMonth(year, month) {
+    wx.showLoading({
+      mask: true
+    })
     // 承载所有操作的 promise 的数组
     let tasks = []
-    let year = '2020', month = '6'
-    for (let i = 1; i < 30; i++) {
-      let dateString = year + '-' + month + '-' + i;
+    this.loadTaskForCollection(tasks, 'repair-bill', year, month)
+    this.loadTaskForCollection(tasks, 'purchase-bill', year, month)
+    // 等待执行所有
+    Promise.all(tasks).then((values) => {
+      wx.hideLoading()
+      // console.log(values);
+      let list = []
+      values.forEach(total => {
+        if (total.result.list.length > 0) {
+          list.push(total.result.list[0].totalAmount)
+        } else {
+          list.push(0)
+        }
+      });
+      // list的前面一半是收入，后面一半是支出，处理一下，算出收益
+      let statisticList = []
+      for (let i = 0; i < list.length / 2; i++) {
+        statisticList.push({
+          income: list[i],
+          expenditure: list[i + list.length / 2],
+          profit: list[i] - list[i + list.length / 2],
+        })
+      }
+      this.setData({
+        statisticList: statisticList
+      })
+    });
+  },
+
+  loadTaskForCollection(tasks, collectionName, year, month) {
+    let daysCount = this.daysCountInYearAndMonth(year, month)
+    for (let i = 0; i < daysCount; i++) {
+      let dateString = year + '-' + month + '-' + (i + 1);
       let fromDateString = dateString + ' 00:00:00 GMT +8'
       let toDateString = dateString + ' 23:59:59 GMT +8'
       const promise = wx.cloud.callFunction({
         name: 'period-amount',
         data: {
+          collectionName: collectionName,
           fromDate: new Date(fromDateString),
           toDate: new Date(toDateString)
         }
       })
       tasks.push(promise)
     }
-    // 等待执行所有
-    Promise.all(tasks).then((values) => {
-      console.log(values[10].result.list[0].totalAmount);
-    });
-
-    // console.log(results)
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  daysCountInYearAndMonth(year, month) {
+    switch (month) {
+      case 1:
+        return 31
+      case 2:
+        return this.isLeapYear(year) ? 29 : 28
+      case 3:
+        return 31
+      case 4:
+        return 30
+      case 5:
+        return 31
+      case 6:
+        return 30
+      case 7:
+        return 31
+      case 8:
+        return 31
+      case 9:
+        return 30
+      case 10:
+        return 31
+      case 11:
+        return 30
+      case 12:
+        return 31
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  isLeapYear(year) {
+    return year % 400 == 0 || (year % 4 == 0 && (year % 100 != 0))
   }
 })
