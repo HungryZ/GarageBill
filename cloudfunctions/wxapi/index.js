c691412683a4917b4fafde01630f17194626c8a0
zhanghaichuan <924320752@qq.com>
2022-10-18 20:54:26 +0800

    weui update

---
+++ cloudfunctions/wxapi/index.js
@@ -0,0 +1,20 @@
+const cloud = require('wx-server-sdk')
+cloud.init({
+  env: cloud.DYNAMIC_CURRENT_ENV,
+})
+exports.main = async (event, context) => {
+  console.log('imageUrl: ' + event.imageUrl)
+  try {
+    const result = await cloud.openapi.ocr.driverLicense({
+        "type": 'photo',
+        // img: {
+        //   contentType: 'image/png',
+        //   value: event.imageBuffer
+        // }
+        "imgUrl": event.imageUrl
+      })
+    return result
+  } catch (err) {
+    return err
+  }
+}
