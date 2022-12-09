14a8f7bc1f7889921648b934e91ba1018515a014
1 <1>
2022-12-09 17:20:50 +0800

    图片压缩、重绘 & 日志

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,5 +1,7 @@
 //index.js
 const app = getApp()
+// 上传图片大小限制，单位字节 Byte
+const IMAGE_SIZE_LIMIT = 512 * 1024
 
 Page({
   data: {
@@ -54,12 +56,7 @@ Page({
 
   },
 
-
-  cameraButtonClicked() {
-    wx.showLoading({
-      title: '车牌识别中',
-      mask: true
-    })
+  scanItemClicked() {
     let that = this
     wx.chooseMedia({
       count: 1,
@@ -68,39 +65,22 @@ Page({
       sizeType: ['compressed'],
       camera: 'back',
       success(res) {
-        let fileSizeInKB = res.tempFiles[0].size / 1000
-        console.log('fileSizeInKB: ' + fileSizeInKB)
-        let quality = parseInt(200.0 / fileSizeInKB * 100)
-        console.log('quality: ' + quality)
-        wx.compressImage({
-          src: res.tempFiles[0].tempFilePath,
-          quality: quality,
-          compressedWidth: 100, // px
-          success(res) {
-            wx.getFileSystemManager().readFile({
-              filePath: res.tempFilePath,
-              encoding: 'base64',
-              success(res) {
-                let imageBase64 = 'data:image/jpeg;base64,' + res.data
-                that.requestOCR(imageBase64)
-              },
-              fail(err) {
-                wx.showToast({
-                  icon: 'none',
-                  title: '解码失败'
-                })
-              },
-            })
-          },
-          fail(err) {
-            wx.showToast({
-              icon: 'none',
-              title: '压缩失败'
-            })
-          },
+        wx.showLoading({
+          title: '车牌识别中',
+          mask: true
+        })
+        that.data.compressStartTime = performance.now()
+        that.handleImage(res.tempFiles[0], (succeed, imageBase64) => {
+          that.data.log += 'handleImage done cost:' + (performance.now() - that.data.compressStartTime).toFixed(0) + ' |\n'
+          if (succeed) {
+            let imageBase64Full = 'data:image/jpeg;base64,' + imageBase64
+            that.requestOCR(imageBase64Full)
+          }
         })
       },
       fail(err) {
+        that.data.log += 'chooseMedia fail:' + err + ' |\n'
+        console.log('chooseMedia fail:' + err)
         wx.showToast({
           icon: 'none',
           title: '拍照失败'
@@ -109,7 +89,145 @@ Page({
     })
   },
 
+  handleImage(tempFile, complete) {
+    this.data.log = ''
+    this.data.log += Date() + ' |\n'
+    this.data.log += 'IMAGE_SIZE_LIMIT:' + (IMAGE_SIZE_LIMIT / 1024).toFixed(0) + 'KB' + ' |\n'
+    this.data.log += 'origin image size:' + (tempFile.size / 1024).toFixed(0) + ' |\n'
+    console.log('origin image size:' + (tempFile.size / 1024).toFixed(0))
+    if (tempFile.size <= IMAGE_SIZE_LIMIT) {
+      this.data.log += 'origin image size ok |\n'
+      console.log('origin image size ok')
+      wx.getFileSystemManager().readFile({
+        filePath: tempFile.tempFilePath,
+        encoding: 'base64',
+        success(res) {
+          complete(true, res.data)
+        },
+        fail(err) {
+          complete(false)
+        }
+      })
+    } else {
+      this.data.log += 'origin image need compress |\n'
+      console.log('origin image need compress')
+      this.compressImageByWX(IMAGE_SIZE_LIMIT, tempFile.tempFilePath, complete)
+    }
+  },
+
+  compressImageByWX(sizeLimit, filePath, complete, times = 0) {
+    let that = this
+    this.data.log += 'compressImageByWX start times:' + times + ' |\n'
+    console.log('compressImageByWX start times:' + times)
+    wx.compressImage({
+      src: filePath,
+      quality: 35,
+      compressedWidth: 500, // px
+      success(compRes) {
+        wx.getFileSystemManager().readFile({
+          filePath: compRes.tempFilePath,
+          encoding: 'base64',
+          success(res) {
+            var compressedImageSize = (res.data.length - (res.data.length / 8) * 2).toFixed(3);
+            that.data.log += 'compressedImageSize:' + (compressedImageSize / 1024).toFixed(0) + ', times:' + times + ' |\n'
+            console.log('compressedImageSize:' + (compressedImageSize / 1024).toFixed(0) + ', times:' + times)
+            if (compressedImageSize <= sizeLimit) {
+              that.data.log += 'compress done times:' + times + ' |\n'
+              console.log('compress done times:' + times)
+              complete(true, res.data)
+            } else if (times < 1) {
+              that.data.log += 'compress again times:' + times + ' |\n'
+              console.log('compress again times:' + times)
+              that.compressImageByWX(sizeLimit, compRes.tempFilePath, complete, times + 1)
+            } else {
+              that.data.log += 'start convas redraw times:' + times + ' |\n'
+              console.log('start convas redraw times:' + times)
+              that.redrawImageByCanvas(sizeLimit, compRes.tempFilePath, complete)
+            }
+          },
+          fail(err) {
+            that.data.log += 'compressImageByWX readFile fail:' + err + ' |\n'
+            console.log('compressImageByWX readFile fail:' + err)
+            wx.showToast({
+              icon: 'none',
+              title: '解码失败'
+            })
+            complete(false)
+          },
+        })
+      },
+      fail(err) {
+        that.data.log += 'compressImageByWX compressImage fail:' + err + ' |\n'
+        console.log('compressImageByWX compressImage fail:' + err)
+        wx.showToast({
+          icon: 'none',
+          title: '压缩失败'
+        })
+        complete(false)
+      },
+    })
+  },
+
+  redrawImageByCanvas(sizeLimit, tempFilePath, complete) {
+    this.data.log += 'redrawImageByCanvas start |\n'
+    console.log('redrawImageByCanvas start')
+    let that = this;
+    wx.getImageInfo({
+      src: tempFilePath,
+      success(imgres) {
+        let ratio = imgres.width / imgres.height
+        // width * (width / ratio) * 4 = sizeLimit * 16 // 乘16是因为JPEG格式会压缩，压缩比例不固定，这里假设1:16
+        let drawWidth = Math.sqrt(sizeLimit * 16 * ratio / 4).toFixed(2)
+        let drawHeight = (drawWidth / ratio).toFixed(2)
+        that.setData({
+          drawWidth: imgres.width,
+          drawHeight: imgres.height,
+        })
+        that.data.log += 'redrawImageByCanvas ' + "originImgwidth:" + imgres.width + ", originImgheight:" + imgres.height + ", ratio:" + ratio + ", drawWidth:" + drawWidth + ", drawHeight:" + drawHeight + ' |\n'
+        console.log('redrawImageByCanvas ' + "originImgwidth:" + imgres.width + ", originImgheight:" + imgres.height + ", ratio:" + ratio + ", drawWidth:" + drawWidth + ", drawHeight:" + drawHeight)
+        
+        const ctx = wx.createCanvasContext('attendCanvasId'); //canvas id
+        ctx.drawImage(tempFilePath, 0, 0, imgres.width, imgres.height);
+        ctx.draw(false, function () {
+          wx.canvasToTempFilePath({
+            canvasId: 'attendCanvasId',
+            destWidth: drawWidth,
+            destHeight: drawHeight,
+            fileType: 'jpg', //目标文件的类型，这里可以根据实际情况来，
+            quality: 0.8, //图片的质量，目前仅对 jpg 有效。取值范围为 (0, 1]，不在范围内时当作 1.0 处理。
+            success(s) {
+              wx.getFileSystemManager().readFile({
+                filePath: s.tempFilePath,
+                encoding: 'base64',
+                success(res) {
+                  var imageSize = ((res.data.length - (res.data.length / 8) * 2) / 1024).toFixed(3);
+                  that.data.log += 'redrawImageByCanvas done size:' + imageSize + ' |\n'
+                  console.log('redrawImageByCanvas done size:' + imageSize)
+                  complete(true, res.data)
+                },
+                fail(err) {
+                  wx.showToast({
+                    icon: 'none',
+                    title: '解码失败'
+                  })
+                  complete(false)
+                },
+              })
+            },
+            fail(err) {
+              complete(false)
+            }
+          });
+        });
+      }
+    })
+  },
+
   requestOCR(imageBase64) {
+    this.data.OCRStartTime = performance.now()
+    this.data.log += 'requestOCR start' + ' |\n'
+    console.log('requestOCR start')
+    let that = this
     wx.request({
       url: 'https://klogan.cn/zhc/ocr/vehicleLicense',
       method: "POST",
@@ -120,19 +238,61 @@ Page({
         'imageBase64': imageBase64,
       },
       success(res) {
+        console.log(res)
         wx.hideLoading()
-        console.log(res.data)
-        wx.navigateTo({
-          url: '../bill/search/search?itemType=0&selectedTypeIndex=0&inputVal=' + res.data.number,
-        })
+        if (res.data.number) {
+          that.data.log += 'requestOCR success:' + JSON.stringify(res.data) + ' |\n'
+          console.log('requestOCR success:' + JSON.stringify(res.data))
+          let nowTime = performance.now()
+          that.data.log += 'requestOCR done cost:' + (nowTime - that.data.OCRStartTime).toFixed(0) + ' |\n'
+          console.log('requestOCR done cost:' + (nowTime - that.data.OCRStartTime).toFixed(0))
+          that.data.log += 'OCR Flow done total cost:' + (nowTime - that.data.compressStartTime).toFixed(0) + ' |\n'
+          console.log('OCR Flow done total cost:' + (nowTime - that.data.compressStartTime).toFixed(0))
+          that.uploadLog()
+          wx.navigateTo({
+            url: '../bill/search/search?itemType=0&selectedTypeIndex=0&inputVal=' + res.data.number,
+          })
+        } else {
+          wx.showToast({
+            icon: 'none',
+            title: '识别失败'
+          })
+        }
       },
       fail(error) {
-        console.log(error)
+        that.data.log += 'requestOCR fail:' + error + ' |\n'
+        console.log('requestOCR fail:' + error)
         wx.showToast({
           icon: 'none',
           title: '识别失败'
         })
       },
+    })
+  },
+
+  uploadLog() {
+    wx.cloud.callFunction({
+      name: 'add',
+      data: {
+        collectionName: 'log',
+        data: {
+          log: this.data.log
+        }
+      },
+      success: res => {
+        console.log('[云函数] [add] 调用成功：', res.result)
+        wx.showToast({
+          icon: 'success',
+          title: '创建成功',
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [add] 调用失败', err)
+        wx.showToast({
+          icon: 'none',
+          title: '请求失败'
+        })
+      }
     })
   },
 })
