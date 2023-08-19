38ecf1aa6b8d6a2903dce7f2a3ce44b468b2ad01
zhanghaichuan <924320752@qq.com>
2023-08-19 17:05:32 +0800

    统计功能改造

--- cloudfunctions/period-amount/index.js
+++ cloudfunctions/period-amount/index.js
@@ -3,30 +3,93 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 const db = cloud.database()
-const $ = db.command.aggregate
+const _ = db.command
+const $ = _.aggregate
 
+/*
+入参
+collectionName
+year
+month
+hideEmptyDays 隐藏数据为0的天
+*/
 // 云函数入口函数
 exports.main = async (event, context) => {
-  return db
+  const boundaries = this.generateBoundaries(event.year, event.month)
+  let fromDate = new Date(event.year, event.month - 1, 1)
+  let toDate = new Date(event.year, event.month, 1)
+  let result = await db
     .collection(event.collectionName)
     .aggregate()
+    // 直接使用 match 查询 date 无效，应该是bug，所以这里先加字段，然后筛选
+    // .match({
+    //   date: _.and(_gte(fromDate), _.lt(toDate))
+    // })
     .project({
-      // and 操作好像有问题，不论是比较date还是int，都不能得到正确的结果
-      //（测试的结果是and返回值跟第一个表达式结果相同，而不是文档上写的“and 仅在所有表达式都返回 true 时返回 true，否则返回 false”）
-      // 只好注释掉下面这行，把两个条件拆开，再到 match 里筛选
-      // matched: $.and($.gte(['$date', date1]), $.lt(['$date', date2])),
-
-      gte: $.gte(['$date', event.fromDate]),
-      lt: $.lt(['$date', event.toDate]),
-      amount: '$amount',
+      date: 1,
+      amount: 1,
+      matched: $.and($.gte(['$date', fromDate]), $.lt(['$date', toDate])),
     })
     .match({
-      gte: true,
-      lt: true
+      matched: true
     })
-    .group({
-      _id: null,
-      totalAmount: $.sum('$amount')
+    .bucket({
+      groupBy: '$date',
+      boundaries: boundaries,
+      default: 'other',
+      output: {
+        totalAmount: $.sum('$amount'),
+        count: $.sum(1),
+        ids: $.push('$_id')
+      }
     })
     .end()
+
+  var resultList = result.list
+
+  // 补全数据为0的天
+  if (!event.hideEmptyDays) {
+    var filledResultList = []
+    boundaries.forEach(boundary => {
+      var dayAmount
+      result.list.forEach(item => {
+        if (new Date(item._id).valueOf() === boundary.valueOf()) {
+          dayAmount = item
+          // forEach 如何结束？
+        }
+      })
+      if (!dayAmount) {
+        dayAmount = {
+          _id: boundary,
+          totalAmount: 0,
+          count: 0,
+          ids: []
+        }
+      }
+      filledResultList.push(dayAmount)
+    })
+    resultList = filledResultList
+  }
+
+  return resultList
+}
+
+exports.generateBoundaries = (year, month) => {
+  var boundaries = []
+  let daysCount = this.daysCount(year, month)
+  for (let i = 0; i < daysCount; i++) {
+    boundaries.push(new Date(year, month - 1, i + 1))
+  }
+  return boundaries
+}
+
+exports.daysCount = (year, month) => {
+  /*
+    yyyy:四位数表示的年份
+    M:用整数表示月份，从0(1月)到11(12月）
+    d:表示一个月中的第几天，从1到31
+
+    这里 month 是从0开始的，所以 new Date(year, month, 0) 表示下个月0号，即当月的最后一天
+  */
+  return new Date(year, month, 0).getDate()
 }
--- miniprogram/pages/statistics/statistics.js
+++ miniprogram/pages/statistics/statistics.js
@@ -12,101 +12,38 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
+  onLoad: function (options) {},
 
   bindDateChange(e) {
     this.setData({
       dateString: e.detail.value
     })
     let array = e.detail.value.split('-')
-    this.requestTotalForYearAndMonth(Number(array[0]), Number(array[1]))
+    this.requestTotalAmountOfMonth(Number(array[0]), Number(array[1]))
   },
 
-  requestTotalForYearAndMonth(year, month) {
-    wx.showLoading({
-      mask: true
-    })
-    // 承载所有操作的 promise 的数组
-    let tasks = []
-    this.loadTaskForCollection(tasks, 'repair-bill', year, month)
-    this.loadTaskForCollection(tasks, 'purchase-bill', year, month)
-    // 等待执行所有
-    Promise.all(tasks).then((values) => {
-      wx.hideLoading()
-      // console.log(values);
-      let list = []
-      values.forEach(total => {
-        if (total.result.list.length > 0) {
-          list.push(total.result.list[0].totalAmount)
-        } else {
-          list.push(0)
-        }
-      });
-      // list的前面一半是收入，后面一半是支出，处理一下，算出收益
-      let statisticList = []
-      for (let i = 0; i < list.length / 2; i++) {
-        statisticList.push({
-          income: list[i],
-          expenditure: list[i + list.length / 2],
-          profit: list[i] - list[i + list.length / 2],
+  requestTotalAmountOfMonth(year, month) {
+    wx.cloud.callFunction({
+      name: 'period-amount',
+      data: {
+        collectionName: 'repair-bill',
+        year: year,
+        month: month,
+      },
+      success: res => {
+        wx.hideLoading()
+        console.log('[云函数] [period-amount] 调用成功：', res.result)
+        this.setData({
+          statisticList: res.result
+        })
+      },
+      fail: err => {
+        console.error('[云函数] [period-amount] 调用失败', err)
+        wx.showToast({
+          icon: 'none',
+          title: '请求失败'
         })
       }
-      this.setData({
-        statisticList: statisticList
-      })
-    });
+    })
   },
-
-  loadTaskForCollection(tasks, collectionName, year, month) {
-    let daysCount = this.daysCountInYearAndMonth(year, month)
-    for (let i = 0; i < daysCount; i++) {
-      let dateString = year + '-' + month + '-' + (i + 1);
-      let fromDateString = dateString + ' 00:00:00 GMT +8'
-      let toDateString = dateString + ' 23:59:59 GMT +8'
-      const promise = wx.cloud.callFunction({
-        name: 'period-amount',
-        data: {
-          collectionName: collectionName,
-          fromDate: new Date(fromDateString),
-          toDate: new Date(toDateString)
-        }
-      })
-      tasks.push(promise)
-    }
-  },
-
-  daysCountInYearAndMonth(year, month) {
-    switch (month) {
-      case 1:
-        return 31
-      case 2:
-        return this.isLeapYear(year) ? 29 : 28
-      case 3:
-        return 31
-      case 4:
-        return 30
-      case 5:
-        return 31
-      case 6:
-        return 30
-      case 7:
-        return 31
-      case 8:
-        return 31
-      case 9:
-        return 30
-      case 10:
-        return 31
-      case 11:
-        return 30
-      case 12:
-        return 31
-    }
-  },
-
-  isLeapYear(year) {
-    return year % 400 == 0 || (year % 4 == 0 && (year % 100 != 0))
-  }
 })
